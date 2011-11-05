4312313d53eaf9b5a4f9d3eace2f4cf34af79592
Matías Sebastián González <matias87lp@gmail.com>
2011-11-05 15:08:52 -0200

    Modificaciones realizadas para los issues #4, #8, #9, #10

---
+++ GranelesWeb/src/main/java/com/orco/graneles/domain/miscelaneos/AdicionalTarea.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orco.graneles.domain.miscelaneos;
+
+/**
+ *
+ * @author orco
+ */
+public class AdicionalTarea {
+
+    public static final int ID_LISTA = 14;
+    
+    public static final int INSALUBRE = 140;
+    public static final int PELIGROSA = 141;
+    public static final int PELIGROSA2 = 142;
+    public static final int PRODUCTIVA = 143;
+    
+}
