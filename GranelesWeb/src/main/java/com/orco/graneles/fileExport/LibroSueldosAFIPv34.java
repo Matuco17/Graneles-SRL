9499a6dcb0da1017103c278172392455ddeacd0c
Matías Sebastián González <matias87lp@gmail.com>
2012-09-15 14:12:17 -0300

    #62, terminado

--- GranelesWeb/src/main/java/com/orco/graneles/fileExport/LibroSueldosAFIPv34.java
+++ GranelesWeb/src/main/java/com/orco/graneles/fileExport/LibroSueldosAFIPv34.java
@@ -54,26 +54,23 @@ public class LibroSueldosAFIPv34 extends ExportadorGenerico<Sueldo> {
             linea.append("00");
         }
         
-        //Codigo de situacion (3)
-        linea.append(" 1"); //2
+        //Codigo de situacion (2)
+        linea.append("01");
+     
+        //Codigo de Condicion (2)
+        linea.append("01");
         
-        //Codigo de Condicion (3)
-        linea.append(" 1"); //2
-        
-        //Codigo de Actividad (3)
-        linea.append("49"); //2
+        //Codigo de Actividad (2)
+        linea.append("49"); 
         
         //Codigo de zona
-        linea.append("07");//2 
+        linea.append("07");
         
         //Porcentaje Aporte Adicional SS (5)
         linea.append(" 0.00"); 
         
-        //Porcentaje de Reduccion (3)
-        linea.append(" ");//1 
-        
         //Codigo modalidad de contratacion (3)
-        linea.append("12"); //2 
+        linea.append("012"); 
         
         //Codigo de Obra social (6)
         linea.append(sdo.getPersonal().getObraSocial().getCodigoAfip());
@@ -81,53 +78,94 @@ public class LibroSueldosAFIPv34 extends ExportadorGenerico<Sueldo> {
         //Cantidad de Adherentes (2)
         linea.append("00");
         
-        //Remuneration Total (15)
+        //Remuneration Total (9)
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
-        //Remuneracion Imponible Aportes (15)
+        //Remuneracion Imponible 1 (9)
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
+        //Asignaciones Familiares Pagadas (9)
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Importe Aporte Voluntario
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Importe Adicional OS
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Importe Excedentes Aportes SS
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Importe Excedentes Aportes OS
         linea.append(formatearImporte9(BigDecimal.ZERO));
         
         //Provincia Localidad (50)
         linea.append(StringUtils.rightPad("Buenos Aires - Resto de la Provincia", 50, " "));
         
-        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
-        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
+        //Remuneracion Imponible 2
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
-        linea.append("00F");
+        //Remuneracion Imponible 3
+        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
+        //Remuneracion Imponible 4
+        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
+        
+        //Codigo Siniestrado (2)
+        linea.append("00");
+        
+        //Marca Correspondiente Reducción (1)
+        linea.append("F");
+        
+        //Capital de Recomposición LRT
         linea.append(formatearImporte9(BigDecimal.ZERO));
         
+        //Tipo Empresa (1)
         linea.append("1"); //0 o 1 
         
+        //Aporte Adicional OS
         linea.append(formatearImporte9(BigDecimal.ZERO));
         
-        //TODO: por ahora le pongo un cero!!!!!!
-        linea.append("0"); //0 o 1 
+        //Regimen 
+        linea.append("1"); //0 o 1 
         
-        linea.append(" 1 1 0 0 0 0");
+        //Situacion Revista 1 (2)
+        linea.append("01");
         
-        //Suedo = Horas habiles monto
+        //Dia Inicio Situacion Revista 1 (2)
+        linea.append("01");
+        
+        //Situacion Revista 2 (2)
+        linea.append("00");
+        
+        //Dia Inicio Situacion Revista 2 (2)
+        linea.append("00");
+        
+        //Situacion Revista 3 (2)
+        linea.append("00");
+        
+        //Dia Inicio Situacion Revista 3 (2)
+        linea.append("00");
+        
+        
+        //Sueldo más adicionales Sueldo = Horas habiles monto
         linea.append(formatearImporte9(
                     SueldoFacade.obtenerMontoXConcepto(sdo, TipoValorConcepto.HORAS_HABILES).add(                    
                     SueldoFacade.obtenerMontoXConcepto(sdo, TipoValorConcepto.PAGOS_ACCIDENTADO_ART))));
         
-        linea.append(formatearImporte9(BigDecimal.ZERO));
+        //SAC
+        linea.append(formatearImporte9(SueldoFacade.obtenerMontoXConcepto(sdo, TipoValorConcepto.SAC)));
         
-        //Suedo = Horas habiles monto
+        //Horas Extras Suedo = Horas habiles monto
         linea.append(formatearImporte9(SueldoFacade.obtenerMontoXConcepto(sdo, TipoValorConcepto.HORAS_EXTRAS)));
         
-        linea.append(formatearImporte9(BigDecimal.ZERO));
+        //Zona desfavorable
         linea.append(formatearImporte9(BigDecimal.ZERO));
         
-        linea.append("       "); //7
+        //Vacaciones
+        linea.append(formatearImporte9(SueldoFacade.obtenerMontoXConcepto(sdo, TipoValorConcepto.VACACIONES)));
         
+        //Cantidad dias trabajados (9)
         if (sdo.getPersonal().getTipoRecibo().getId().equals(TipoRecibo.HORAS)){
             int horas = SueldoFacade.obtenerCantidadXConcepto(sdo, TipoValorConcepto.HORAS_HABILES).intValue();
             horas += SueldoFacade.obtenerCantidadXConcepto(sdo, TipoValorConcepto.HORAS_EXTRAS).intValue();
@@ -136,37 +174,64 @@ public class LibroSueldosAFIPv34 extends ExportadorGenerico<Sueldo> {
             
             dias = Math.min(dias, diasMes);
             
-            linea.append(StringUtils.right("  " + 
+            linea.append(StringUtils.right("000000000" + 
                                     String.valueOf(dias)
-                                 , 2));
+                                 , 9));
         } else {
-            linea.append(StringUtils.right("  " + 
+            linea.append(StringUtils.right("000000000" + 
                                     (SueldoFacade.obtenerCantidadXConcepto(sdo, TipoValorConcepto.DIAS_TRABAJO).toBigInteger()).toString()
-                                    , 2));
+                                    , 9));
         }
         
+        //Remuneración Imponible 5
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
+        //Trabajador convensionado (1=Si 01=No)
+        linea.append("F");
+        
+        //Remuneracion Imponible 6
+        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
+        
+        //Tipo Operacion
         linea.append("0");
         
+        //Adicionales
+        linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Premios
+        linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Remuneracion Dec 788/05 Remuneracion Imponible 8
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
-        linea.append(" ");
-        
-        linea.append(formatearImporte9(BigDecimal.ZERO));
-        linea.append(formatearImporte9(BigDecimal.ZERO));
-        
+        //Remuneracion Imponible 7
         linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
         
-        linea.append(formatearImporte9(BigDecimal.ZERO));
-        
-        linea.append(StringUtils.right("   " + 
+        //Cantidad Horas extras (3)
+        linea.append(StringUtils.right("000" + 
                                     (SueldoFacade.obtenerCantidadXConcepto(sdo, TipoValorConcepto.HORAS_EXTRAS).toBigInteger()).toString()
                                     , 3));
         
+        //Conceptos No Remunerativos
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Maternidad
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Rectificacion de Remuneracion
         linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Remuneracion Imponible 9
+        linea.append(formatearImporte9(sdo.getTotalRemunerativo(true)));
+        
+        //Contribucion tarea diferencial
+        linea.append(formatearImporte9(BigDecimal.ZERO));
+        
+        //Horas Trabajadas (3)
+        linea.append("000"); //Obs: no pongo nada ya que lo tengo entre las horas extras y los dias trabajados
+        
+        //Seguro colectivo de vida Obligatorio (1)
+        linea.append("T");
         
         return linea.toString();
     }
