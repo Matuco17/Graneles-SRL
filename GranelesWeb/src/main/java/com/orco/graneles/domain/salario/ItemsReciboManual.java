14e5bfce2ab75f74027d72bea6060fca508df785
Matías Sebastián González <matias87lp@gmail.com>
2013-09-15 09:13:19 -0300

    #87, Creacion de entidades

---
+++ GranelesWeb/src/main/java/com/orco/graneles/domain/salario/ItemsReciboManual.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orco.graneles.domain.salario;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author orco
+ */
+@Entity
+@Table(name = "items_recibo_manual", catalog = "graneles", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ItemsReciboManual.findAll", query = "SELECT i FROM ItemsReciboManual i"),
+    @NamedQuery(name = "ItemsReciboManual.findById", query = "SELECT i FROM ItemsReciboManual i WHERE i.id = :id"),
+    @NamedQuery(name = "ItemsReciboManual.findByCantidad", query = "SELECT i FROM ItemsReciboManual i WHERE i.cantidad = :cantidad"),
+    @NamedQuery(name = "ItemsReciboManual.findByValor", query = "SELECT i FROM ItemsReciboManual i WHERE i.valor = :valor")})
+public class ItemsReciboManual implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(precision = 19, scale = 2)
+    private BigDecimal cantidad;
+    
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal valor;
+    
+    @JoinColumn(name = "recibo", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private ReciboManual recibo;
+
+    public ItemsReciboManual() {
+    }
+
+    public ItemsReciboManual(Long id) {
+        this.id = id;
+    }
+
+    public ItemsReciboManual(Long id, BigDecimal valor) {
+        this.id = id;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(BigDecimal cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public ReciboManual getRecibo() {
+        return recibo;
+    }
+
+    public void setRecibo(ReciboManual recibo) {
+        this.recibo = recibo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ItemsReciboManual)) {
+            return false;
+        }
+        ItemsReciboManual other = (ItemsReciboManual) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.orco.graneles.domain.salario.ItemsReciboManual[ id=" + id + " ]";
+    }
+    
+}
---
+++ GranelesWeb/src/main/java/com/orco/graneles/domain/salario/ReciboManual.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orco.graneles.domain.salario;
+
+import com.orco.graneles.domain.personal.Personal;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author orco
+ */
+@Entity
+@Table(name = "recibo_manual", catalog = "graneles", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ReciboManual.findAll", query = "SELECT r FROM ReciboManual r"),
+    @NamedQuery(name = "ReciboManual.findById", query = "SELECT r FROM ReciboManual r WHERE r.id = :id")})
+public class ReciboManual implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    
+    @JoinColumn(name = "periodo", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Periodo periodo;
+    
+    @JoinColumn(name = "personal", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Personal personal;
+   
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch= FetchType.EAGER, mappedBy="recibo", orphanRemoval = true)
+    private Collection<ItemsReciboManual> itemsReciboManualCollection;
+
+    public ReciboManual() {
+    }
+
+    public ReciboManual(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
+    }
+
+    public Personal getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(Personal personal) {
+        this.personal = personal;
+    }
+    
+    @XmlTransient
+    public Collection<ItemsReciboManual> getItemsReciboManualCollection() {
+        return itemsReciboManualCollection;
+    }
+
+    public void setItemsReciboManualCollection(Collection<ItemsReciboManual> itemsReciboManualCollection) {
+        this.itemsReciboManualCollection = itemsReciboManualCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ReciboManual)) {
+            return false;
+        }
+        ReciboManual other = (ReciboManual) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.orco.graneles.domain.salario.ReciboManual[ id=" + id + " ]";
+    }
+    
+}
